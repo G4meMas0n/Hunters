deaa95aebf29fd29bf4323c008fbb9ceeb7c2306
G4meMason <g4memas0n@gmail.com>
2020-09-06 21:12:42 +0200

    Adds message translations construct

--- Hunters/src/main/java/de/g4memas0n/hunters/Hunters.java
+++ Hunters/src/main/java/de/g4memas0n/hunters/Hunters.java
@@ -2,15 +2,23 @@ package de.g4memas0n.hunters;
 
 import de.g4memas0n.hunters.configuration.Settings;
 import de.g4memas0n.hunters.util.logging.BasicLogger;
+import de.g4memas0n.hunters.util.messaging.Messages;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * The Hunters main class.
+ *
+ * @author G4meMas0n
+ * @since Release 1.0.0
+ */
 public final class Hunters extends JavaPlugin {
 
     private final BasicLogger logger;
 
     private Settings settings;
+    private Messages messages;
 
     private boolean loaded;
     private boolean enabled;
@@ -28,6 +36,10 @@ public final class Hunters extends JavaPlugin {
         return this.settings;
     }
 
+    public @NotNull Messages getMessages() {
+        return this.messages;
+    }
+
     @Override
     public void onLoad() {
         if (this.loaded) {
@@ -39,6 +51,9 @@ public final class Hunters extends JavaPlugin {
         this.settings.load();
 
         this.logger.setDebug(this.settings.isDebug());
+
+        this.messages = new Messages(this.getDataFolder(), this.logger);
+        this.messages.setLocale(this.settings.getLocale());
 
         this.loaded = true;
     }
@@ -69,6 +84,19 @@ public final class Hunters extends JavaPlugin {
 
         this.enabled = false;
         this.loaded = false;
+    }
+
+    @Override
+    public void reloadConfig() {
+        this.settings.load();
+
+        this.logger.setDebug(this.settings.isDebug());
+        this.messages.setLocale(this.settings.getLocale());
+    }
+
+    @Override
+    public void saveConfig() {
+        // Disabled, because it is not intended to save the config file, as this breaks the comments.
     }
 
     public @NotNull BukkitTask runTask(@NotNull final Runnable task) {
